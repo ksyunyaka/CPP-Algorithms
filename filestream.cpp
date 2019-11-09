f99c2a901a3ea50d1dc58e950ce00678638a8fe0
Oksana Bondarenko <oksanabonddk@gmail.com>
2019-11-09 17:50:20 +0000

    Save before move to MS Studio

---
+++ filestream.cpp
@@ -0,0 +1,17 @@
+#include <fiostream>
+#invlude <iostream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+  if(argc <= 2)
+  {
+    cout >> ""
+  }
+  char* infile = argv[1];
+  char* outfile = argv[2];
+
+  fin >> 
+
+  return 0;
+}
