32edddf3b2c2e5211231640e88588825f9dced1b
Oksana Bondarenko <oksanabonddk@gmail.com>
2019-11-09 15:17:03 +0000

    Init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+  double a = 2.3;
+  std::cout << "Give a real number" << "\n";
+  std::cin >> a;
+  double r = sin(a);
+  std::cout << "Hello World!" << " sin(" << a
+  return 0;
+}
